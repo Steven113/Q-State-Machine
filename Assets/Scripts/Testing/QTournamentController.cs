d73ddcf039201bb855a00c1c91bb1f8904424a3e
user <user>
2017-05-01 11:51:57 +0200

    Fixed bug where not all genomes were tested the same amount of times.

--- Assets/Scripts/Testing/QTournamentController.cs
+++ Assets/Scripts/Testing/QTournamentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace AssemblyCSharp
 {
@@ -20,37 +21,46 @@ namespace AssemblyCSharp
 
 		public QGraphController graphController;
 
-		int soldierUnderConsideration_i;
-		int soldierUnderConsideration_j;
+		//int soldierUnderConsideration_i;
+		[SerializeField]int soldierUnderConsideration_j = 1;
 
 		public static GameObject [] g_SpawnPoints;
 
 		public void Start(){
 			//graphController.Evolve ();
-
+			Debug.Log("Num combos: "+Mathf.Pow(graphController.numGraphs,soldiers.Length));
 			g_SpawnPoints = spawnPoints;
 
 			for (int i = 0; i < soldiers.Length; ++i) {
-				soldiers [(i) % graphController.numGraphs].Graph = graphController.Graphs [(i + soldierUnderConsideration_i)% graphController.numGraphs];
+				soldiers [(i) % graphController.numGraphs].Graph = graphController.Graphs [(i)% graphController.numGraphs];
 			}
 		}
+
+		Dictionary<QGraph, int> numAssessments = new Dictionary<QGraph, int>();//for debug
 
 		public void Update(){
 			timeSinceRoundStart += Time.deltaTime;
 
 			if (timeSinceRoundStart > roundLength) {
 				timeSinceRoundStart = 0;
+				++soldierUnderConsideration_j;
 
+				if (soldierUnderConsideration_j > Mathf.Pow(graphController.numGraphs,soldiers.Length)) {
 
-				if (soldierUnderConsideration_i >= graphController.numGraphs) {
+					for (int i = 0; i < graphController.numGraphs; ++i) {
+						Debug.Assert (numAssessments [graphController.Graphs [i]] == numAssessments [graphController.Graphs [(i + 1) % graphController.numGraphs]], "Graph " + i+ " tested "+numAssessments [graphController.Graphs [i]] + " times, graph " + ((i + 1) % graphController.numGraphs) + " tested "+numAssessments [graphController.Graphs [(i + 1) % graphController.numGraphs]]+ " times." );
+					}
+
 					graphController.Evolve ();
 
 					Debug.Log ("Evolving.");
 
-					soldierUnderConsideration_i = 0;
+					soldierUnderConsideration_j = 0;
 
 					spawnPoints = Utils.ShuffleArray (spawnPoints);
 				}
+
+				spawnPoints = Utils.ShuffleArray (spawnPoints);
 
 				for (int i = 0; i < dummySoldiers.Length; ++i) {
 					dummySoldiers [i].Graph.ResetCurrentNodeToRoot ();
@@ -72,13 +82,15 @@ namespace AssemblyCSharp
 				GameData.scores = new float[]{ 0, 0 };
 
 				for (int i = 0; i < soldiers.Length; ++i) {
-					if (soldierUnderConsideration_j >= graphController.numGraphs) {
-						soldierUnderConsideration_j = 0;
-						++soldierUnderConsideration_i;
+					int selectedGraph = (int)((soldierUnderConsideration_j / Mathf.Pow (graphController.numGraphs, i))) % graphController.numGraphs;
+					soldiers [i].Graph = graphController.Graphs [selectedGraph];
+					if (!numAssessments.ContainsKey(graphController.Graphs[selectedGraph])){
+						numAssessments.Add(graphController.Graphs[selectedGraph],1);
+					} else {
+						numAssessments[graphController.Graphs[selectedGraph]]+=1;
 					}
+					Debug.Log ("Soldier " + i + " given graph " + selectedGraph);
 
-					soldiers [i].Graph = graphController.Graphs [(soldierUnderConsideration_i * graphController.numGraphs + soldierUnderConsideration_j) % graphController.numGraphs];
-					++soldierUnderConsideration_j;
 				}
 
 
